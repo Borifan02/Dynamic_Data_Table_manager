9414139e3378c5d086aee9caff4c269aade5d9af
Borifan Dabasa <dabasaborifan@gmail.com>
2025-11-04 23:10:56 -0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export', // enables static HTML export
+  images: {
+    unoptimized: true, // disables Next.js image optimization (not supported on GitHub Pages)
+  },
+  basePath: '/Dynamic_Data_Table_manager', // your GitHub repo name
+  assetPrefix: '/Dynamic_Data_Table_manager/', // ensures correct asset paths
+};
 
 module.exports = nextConfig
